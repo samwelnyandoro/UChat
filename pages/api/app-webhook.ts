801ae041a350c012a49c8f285266dc462e8c9ac9
Samwel Nyandoro <36915374+samwelnyandoro@users.noreply.github.com>
2023-10-01 11:55:08 +0300

    Initial commit

---
+++ pages/api/app-webhook.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import camelcase from 'camelcase'
+import lcConfig from '../../livechat.config.json'
+import { DeveloperAppConfig } from '@livechat/developer-sdk'
+
+async function AppWebook(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { clientID, licenseID, event, payload } = req.body
+    if (clientID !== (lcConfig as DeveloperAppConfig).auth?.clientId) {
+      throw new Error('Unauthorized')
+    }
+    const handlerName = camelcase(event) as keyof typeof WebhooksHandlers
+    await WebhooksHandlers[handlerName](licenseID, payload)
+  } catch (error) {
+    const { message } = error as Error
+    console.log('AppWebook -> error: ', message)
+  } finally {
+    res.status(200).end()
+  }
+}
+
+const WebhooksHandlers = {
+  async applicationInstalled(licenseID: number, payload: { applicationID: string }) {
+    console.log('AppWebook -> applicationInstalled', licenseID, payload)
+  },
+
+  async applicationUninstalled(licenseID: number, payload: { applicationID: string }) {
+    console.log('AppWebook -> applicationUninstalled', licenseID, payload)
+  },
+
+  async paymentActivated(licenseID: number, payload: { paymentID: string; quantity: number }) {
+    console.log('AppWebook -> paymentActivated', licenseID, payload)
+  },
+
+  async paymentCollected(licenseID: number, payload: { paymentID: string; total: number }) {
+    console.log('AppWebook -> paymentCollected', licenseID, payload)
+  },
+
+  async paymentCancelled(licenseID: number, payload: { paymentID: string }) {
+    console.log('AppWebook -> paymentCancelled', licenseID, payload)
+  },
+}
+
+export default AppWebook
